ee0d540c4908eeab84ea0aacd813d6f44d2683f6
Masonwp123 <MasonParker868@gmail.com>
2025-10-16 14:51:44 -0400

    - now uses only one class to keep it small - invalid addressing modes are now printed - fixes for address crashes at improper length - token is stripped so whitespace doesn't interfere - labels are now cached and included - better results (ints instead of bools)

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -4,35 +4,49 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.function.Function;
 
 public class pepasm {
+
+    StringBuilder builder;
+    String token;
+    int lineNumber;
 
     // Each command maps to a single character
     // Address mode is 0 or 1, for i or f
     // Ex: LDBA 0x0000,i is D0, LDBA 0x0000,d is D1
 
     //STBA, LDBA, STWA, LDWA, ANDA, ASLA, ASRA, STOP, CPBA, BRNE.
-    public static final Map<String, ITranslatable> commandMapping = Map.ofEntries(
-            Map.entry("CPBA", new AddressableCommand('B')),
-            Map.entry("LDWA", new AddressableCommand('C')),
-            Map.entry("LDBA", new AddressableCommand('D')),
-            Map.entry("STWA", new AddressableCommand('E')),
-            Map.entry("STBA", new AddressableCommand('F')),
-            Map.entry("ANDA", new AddressableCommand('8')),
-            Map.entry("ASLA", new Command("0A")),
-            Map.entry("ASRA", new Command("0C")),
-            Map.entry("STOP", new Command("00")),
-            Map.entry(".END", new Command("zz")),
-            Map.entry("BRNE", new Command("1A")),
-            Map.entry("BREQ", new Command("18"))
+    public static final Map<String, Function<pepasm, Integer>> MAPPING = Map.ofEntries(
+            Map.entry("CPBA", (p) -> addressableCommand(p, "B", List.of('i', 'd'))),
+            Map.entry("LDWA", (p) -> addressableCommand(p, "C", List.of('i', 'd'))),
+            Map.entry("LDBA", (p) -> addressableCommand(p, "D", List.of('i', 'd'))),
+            Map.entry("STWA", (p) -> addressableCommand(p, "E", List.of('d'))),
+            Map.entry("STBA", (p) -> addressableCommand(p, "F", List.of('d'))),
+            Map.entry("ADDA", (p) -> addressableCommand(p, "6", List.of('i', 'd'))),
+            Map.entry("SUBA", (p) -> addressableCommand(p, "7", List.of('i', 'd'))),
+            Map.entry("ANDA", (p) -> addressableCommand(p, "8", List.of('i', 'd'))),
+            Map.entry("BRNE", (p) -> addressableCommand(p, "1A", List.of('i'))),
+            Map.entry("BREQ", (p) -> addressableCommand(p, "18", List.of('i'))),
+            Map.entry("ASLA", (p) -> command(p, "0A")),
+            Map.entry("ASRA", (p) -> command(p, "0C")),
+            Map.entry("STOP", (p) -> command(p, "00")),
+            Map.entry(".END", (p) -> command(p, "zz"))
     );
+
+    public final HashMap<String, String> LABELS = new HashMap<>();
 
     public static void main(String[] args) {
         pepasm p = new pepasm();
-        p.start(args[0]);
+        for (String path : args) {
+            p.assemble(path);
+        }
     }
 
-    public void start(String fileToLoad) {
+    public void assemble(String fileToLoad) {
+
+        // Clear labels for this file
+        LABELS.clear();
 
         // Create directory to load file from
         String projectDirectory = System.getProperty("user.dir");
@@ -45,31 +59,45 @@ public class pepasm {
             return;
         }
 
-        try (Scanner scanner = new Scanner(file)) {
+        try (Scanner scanner = new Scanner(file)){
 
-            StringBuilder builder = new StringBuilder();
+            this.builder = new StringBuilder();
 
-            int lineNumber = 0;
+            this.lineNumber = 0;
             while (scanner.hasNextLine()) {
-                lineNumber++;
+                this.lineNumber++;
+
+                this.token = scanner.nextLine().strip();
+
+                // Check for labels
+                int labelEnd = this.token.indexOf(':');
+                if (labelEnd != -1) {
+                    String label = this.token.substring(0, labelEnd);
+                    // The address of a label is the hex value of the lineNumber minus one * 3
+                    LABELS.put(label, Integer.toHexString((this.lineNumber - 1) * 3));
+
+                    // Remove label from line
+                    this.token = this.token.substring(labelEnd + 1);
+                    this.token = this.token.stripLeading();
+                }
 
                 // Command is always the first four characters
-                String token = scanner.nextLine();
+                if (this.token.isEmpty() || this.token.length() < 4) continue;
                 String command = token.substring(0,4);
 
                 // If command doesn't exist, tell user
-                if (!commandMapping.containsKey(command)) {
+                if (!MAPPING.containsKey(command)) {
                     System.out.println("Unknown command " + command);
                     return;
                 }
                 // translate code and write to string builder, if has an error, return and do not write to file.
-                boolean hasError = commandMapping.get(command).translateCode(builder, token, lineNumber);
-                if (hasError) return;
+                //boolean hasError = commandMapping.get(command).translateCode(builder, token, lineNumber);
+                int result = MAPPING.get(command).apply(this);
+                if (result != 0) return;
             }
 
             // Get directory and change to pepo
             int extension = fileToLoad.lastIndexOf('.');
-            System.out.println(extension);
             String outputFileName = fileToLoad.substring(0, extension);
             outputFileName += ".pepo";
             String outputDirectory = projectDirectory + '\\' + outputFileName;
@@ -77,7 +105,7 @@ public class pepasm {
             // Write to output file
             File outputFile = new File(outputDirectory);
             try (PrintWriter writer = new PrintWriter(outputFile, StandardCharsets.UTF_8);) {
-                writer.write(builder.toString());
+                writer.write(this.builder.toString());
                 System.out.println("Success! Wrote out to " + outputDirectory);
             } catch (IOException e) {
                 System.out.println("Error creating file ");
@@ -87,83 +115,78 @@ public class pepasm {
         }
     }
 
-    public interface ITranslatable {
-        boolean translateCode(StringBuilder builder, String token, int lineNumber);
+    public static int addressableCommand(pepasm p, String token, List<Character> allowedAddressingModes) {
+
+        int endAddressIndex = p.token.indexOf(",");
+
+        // Strip from before the comma and after the command to get the address
+        String addressCode = p.token.substring(4, endAddressIndex);
+        addressCode = addressCode.strip();
+
+        // Check if it is a label first
+        System.out.println(addressCode);
+        if (p.LABELS.containsKey(addressCode)) {
+            // Set code to the label value
+            System.out.println("label " + addressCode + " is " + p.LABELS.get(addressCode));
+            addressCode = p.LABELS.get(addressCode);
+        }
+
+        // Might start with 0x, like 0x0000, machine code doesn't need this, so remove
+        if (addressCode.startsWith("0x")) {
+            addressCode = addressCode.substring(2);
+        }
+
+        if (addressCode.length() > 4) {
+            System.out.println("Invalid Address length for " + addressCode + " at line " + p.lineNumber);
+            return 1;
+        }
+
+        // Ensure addressCode has length of 4
+        while (addressCode.length() < 4) {
+            addressCode = "0" + addressCode;
+        }
+
+        String formattedAddress = addressCode.substring(0, 2) + " " + addressCode.substring(2);
+
+        // Strip from one after the comma and strip leading to get addressing mode
+        String addressType = p.token.substring(endAddressIndex + 1);
+        addressType = addressType.stripLeading();
+
+        boolean direct = false;
+
+        if (addressType.isEmpty()) {
+            System.out.println("No Addressing mode for " + addressCode + " at line " + p.lineNumber);
+            return 1;
+        }
+
+        if (addressType.charAt(0) == 'd') {
+            direct = true;
+        } else if (addressType.charAt(0) != 'i') {
+            System.out.println("Invalid Addressing Mode " + addressType + " at line " + p.lineNumber);
+            return 1;
+        }
+
+        if (!allowedAddressingModes.contains(addressType.charAt(0))) {
+            System.out.println("Illegal addressing mode at line " + p.lineNumber);
+            return 1;
+        }
+
+        // If token is only 1 character, then it needs an addressing mode
+        if (token.length() < 2) {
+            token = token + (direct ? "1" : "0");
+        }
+
+        p.builder.append(token)
+                .append(" ")
+                .append(formattedAddress)
+                .append(" ");
+
+        return 0;
     }
 
-    public static class Command implements ITranslatable {
-        String machineCodeCommand;
-
-        Command(String machineCodeCommand) {
-            this.machineCodeCommand = machineCodeCommand;
-        }
-
-        @Override
-        public boolean translateCode(StringBuilder builder, String token, int lineNumber) {
-            builder.append(machineCodeCommand);
-            builder.append(" ");
-            return false;
-        }
-    }
-
-    public record Address(String memoryAddress, boolean direct) {
-        public String toObjectCode(Character machineCodeCommand) {
-            return machineCodeCommand + (direct ? "1" : "0") + " " + memoryAddress + " ";
-        }
-    }
-
-    public static class AddressableCommand implements ITranslatable {
-        Character machineCodeCommand;
-
-        AddressableCommand(Character machineCodeCommand) {
-            this.machineCodeCommand = machineCodeCommand;
-        }
-
-        @Override
-        public boolean translateCode(StringBuilder builder, String token, int lineNumber) {
-            int endAddressIndex = token.indexOf(",");
-
-            // Strip from before the comma and after the command to get the address
-            String addressCode = token.substring(4, endAddressIndex);
-            addressCode = addressCode.stripLeading();
-
-            // Might start with 0x, like 0x0000, machine code doesn't need this, so remove
-            if (addressCode.startsWith("0x")) {
-                addressCode = addressCode.substring(2);
-            }
-
-            String formattedAddress;
-            if (addressCode.length() == 2) {
-                formattedAddress = "00 " + addressCode;
-            } else if (addressCode.length() == 4) {
-                formattedAddress = addressCode.substring(0, 2) + " " + addressCode.substring(2, 4);
-            } else {
-                System.out.println("Invalid Address length for " + addressCode + " at line " + lineNumber);
-                return true;
-            }
-
-            // Strip from one after the comma and strip leading to get addressing mode
-            String addressType = token.substring(endAddressIndex + 1);
-            addressType = addressType.stripLeading();
-
-            boolean direct = false;
-
-            if (addressType.isEmpty()) {
-                System.out.println("No Addressing mode for " + addressCode + " at line " + lineNumber);
-                return true;
-            }
-
-            if (addressType.charAt(0) == 'd') {
-                direct = true;
-            } else if (addressType.charAt(0) != 'i') {
-                System.out.println("Invalid Addressing Mode " + addressType + " at line " + lineNumber);
-                return true;
-            }
-
-            Address address = new Address(formattedAddress, direct);
-            builder.append(address.toObjectCode(machineCodeCommand));
-
-            return false;
-        }
+    public static int command(pepasm p, String token) {
+        p.builder.append(token);
+        p.builder.append(" ");
+        return 0;
     }
 }
